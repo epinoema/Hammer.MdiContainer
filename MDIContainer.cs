ea9fd5ab1e9e3ec444d6fd15a6d78195d36ffe65
ʌἁɀɑƿѻѕ <larjohn@gmail.com>
2016-01-24 14:26:21 +0200

    First Commit

---
+++ MDIContainer.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using Hammer.MDIContainer.Control.Events;
+
+namespace Hammer.MDIContainer.Control
+{
+    public sealed class MdiContainer : Selector
+   {
+      private IList InternalItemSource { get; set; }
+      internal int MinimizedWindowsCount { get; private set; }
+
+      static MdiContainer()
+      {
+         DefaultStyleKeyProperty.OverrideMetadata(typeof(MdiContainer), new FrameworkPropertyMetadata(typeof(MdiContainer)));
+      }
+
+      protected override DependencyObject GetContainerForItemOverride()
+      {
+         return new MdiWindow();
+      }
+
+      protected override void PrepareContainerForItemOverride(DependencyObject element, object item)
+      {
+         var window = element as MdiWindow;
+         if (window != null)
+         {
+            window.IsCloseButtonEnabled = InternalItemSource != null;
+            window.FocusChanged += OnWindowFocusChanged;
+            window.Closing += OnWindowClosing;
+            window.WindowStateChanged += OnWindowStateChanged;
+            window.Initialize(this);
+
+            Canvas.SetTop(window, 32);
+            Canvas.SetLeft(window, 32);
+
+            window.Focus();
+         }
+
+         base.PrepareContainerForItemOverride(element, item);
+      }      
+
+      private void OnWindowStateChanged(object sender, WindowStateChangedEventArgs e)
+      {
+         if (e.NewValue == WindowState.Minimized)
+         {
+            MinimizedWindowsCount++;
+         }
+         else if (e.OldValue == WindowState.Minimized)
+         {
+            MinimizedWindowsCount--;
+         }
+      }      
+
+      private void OnWindowClosing(object sender, RoutedEventArgs e)
+      {
+         var window = sender as MdiWindow;
+         if (window?.DataContext != null)
+         {
+             InternalItemSource?.Remove(window.DataContext);
+
+             // clear
+            window.FocusChanged -= OnWindowFocusChanged;
+            window.Closing -= OnWindowClosing;
+            window.WindowStateChanged -= OnWindowStateChanged;
+            window.DataContext = null;
+         }
+      }
+
+      protected override void OnItemsSourceChanged(IEnumerable oldValue, IEnumerable newValue)
+      {
+         base.OnItemsSourceChanged(oldValue, newValue);
+
+         if (newValue != null && newValue is IList)
+         {
+            InternalItemSource = newValue as IList;
+         }
+      }
+
+      private void OnWindowFocusChanged(object sender, RoutedEventArgs e)
+      {
+         if (((MdiWindow)sender).IsFocused)
+         {
+            SelectedItem = e.OriginalSource;
+
+             foreach (var item in Items)
+             {
+                 if (item != e.OriginalSource)
+                 {
+                     var window = ItemContainerGenerator.ContainerFromItem(item) as MdiWindow;
+                     if (window != null)
+                     {
+                         window.IsSelected = false;
+                         Panel.SetZIndex(window, 0);
+                     }
+                 }
+             }
+         }       
+      }
+   }
+}
